f4aa1dc2108b3d35e4603665c51c15cba852fb13
Johnny Richardson <johnny.c.rich@gmail.com>
2019-08-22 11:56:55 -0400

    Working pathway grid.

--- client/src/app/pathways/grid/grid.component.ts
+++ client/src/app/pathways/grid/grid.component.ts
@@ -18,7 +18,6 @@ export class PathwayGridComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit() {
 
-
     let parentRect = document.getElementById('grid').getBoundingClientRect();
     let parentX = parentRect['x'];
     let parentY = parentRect['y'];
@@ -27,30 +26,52 @@ export class PathwayGridComponent implements OnInit, AfterViewInit {
     let leftCols = document.querySelectorAll('.name svg:not(.odd)');
 
     let draw = SVG('svg-bg').size(parentRect['width'], parentRect['height']);
-    rightCols.forEach((element, i) => {
 
-      let siblingRect = leftCols[i].getBoundingClientRect();
+    function drawVerts(side) {
+
+      // Draw line from each name element to sibling below, if there is one
+      side.forEach((nameEl, i) => {
+
+        if(!side[i+1]) return;
+        let bottomColRect = side[i+1].getBoundingClientRect();
+        
+        let x = (nameEl.getBoundingClientRect()['x'] - parentX) + 20 ;
+        let y = (nameEl.getBoundingClientRect()['y'] - parentY) + 20;
+        let endY = bottomColRect['y'] - parentY;
+        
+        let hLine = draw.line(x, y, x, endY);
+        hLine.stroke({ color: '#ddd', width: 1, linecap: 'round' });
+        
+      });
+
+    }
+
+    drawVerts(leftCols);
+    drawVerts(rightCols);
+
+    leftCols.forEach((element, i) => {
+
+      if(!rightCols[i]) return;
+      let rightColRect = rightCols[i].getBoundingClientRect();
 
       let x = (element.getBoundingClientRect()['x'] - parentX) + 20 ;
       let y = (element.getBoundingClientRect()['y'] - parentY) + 20;
-      let startX = (siblingRect['x'] - parentX) + 20 ;
+      let startX = (rightColRect['x'] - parentX) + 20 ;
       
       let hLine = draw.line(startX, y, x, y);
       hLine.stroke({ color: '#ddd', width: 1, linecap: 'round' });
 
-      // Now draw diagonal line to sibling below, if there is one
-      let drawLeft = i % 2 === 0;
-      let siblingBelow = drawLeft ? leftCols[i+1] : rightCols[i+1]
+      // Now draw diagonal line to sibling below and to side, if there is one
+      let drawRight = i % 2 > 0;
+      let siblingBelow = drawRight ? leftCols[i+1] : rightCols[i+1]
       
       if(siblingBelow) {
 
         // Decide where line ends based on direction
         let siblingBelowRect = siblingBelow.getBoundingClientRect();
-
-        console.log(drawLeft,siblingBelow)
         
-        let startXD = drawLeft ? startX : x; 
-        let endXD = !drawLeft ? startX : x; 
+        let startXD = drawRight ? startX : x; 
+        let endXD = !drawRight ? startX : x; 
         let endY = (siblingBelowRect['y'] - parentY) + 20;
         
         let dLine = draw.line(startXD, y, endXD, endY);
@@ -58,7 +79,7 @@ export class PathwayGridComponent implements OnInit, AfterViewInit {
  
       }
 
-    });0
+    });
 
   }
 
