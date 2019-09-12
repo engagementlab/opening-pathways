cc4c508b29d200bea8b5d2737a8b2bb1518b9e8d
Ralph Drake <ralph_drake@emerson.edu>
2019-09-12 13:30:40 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+set -e
 
 export NVM_DIR=~/.nvm
 source ~/.nvm/nvm.sh
 
-cd client;
-nvm use;
-npm run start-testing;
+cd server
+npm install
+
+cd ..
+
+cd client
+npm install
+nvm use
+npm run start-testing
