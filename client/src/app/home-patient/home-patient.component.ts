dd6e029dbd083c2adef73724fea059228061dfed
Johnny Richardson <johnny.c.rich@gmail.com>
2019-09-12 16:36:34 -0400

    Finally working next/prev dimmer for pathways slider.

--- client/src/app/home-patient/home-patient.component.ts
+++ client/src/app/home-patient/home-patient.component.ts
@@ -30,6 +30,7 @@ export class HomePatientComponent implements OnInit {
       autoHeight: true,
       autoWidth: true,
       dots: false,
+      nav: true,
       items: ismobile.phone ? 1 : 3,
       
       navSpeed: 700,
@@ -71,13 +72,47 @@ export class HomePatientComponent implements OnInit {
     setTimeout(() => {
 
       this._draw.pathways(sliderIndex);
+
+      let stageEl = document.querySelectorAll('.pathway .owl-carousel')[sliderIndex];
+      let overallWidth = stageEl.clientWidth;
+      let sliderWidth = document.querySelectorAll('.pathway .owl-stage')[sliderIndex].clientWidth;
+
+      // Disable arrows if no scrolling
+      if(sliderWidth < overallWidth) {
+        stageEl.parentElement.parentElement.querySelectorAll('nav .arrow').forEach(a => {
+          a.classList.add('disable');
+        });
+      }
       
     }, 50);
 
   }
 
   slideChange(slideInfo: SlidesOutputData, sliderIndex: number) {
-    console.log(slideInfo, slideInfo.slides[slideInfo.slides.length-1])
+
+    // Find out if at end/begin based on class of the built in nav we've hidden, 
+    // since owl output infuriatingly does not return specifics on this
+    
+    let stageEl = document.querySelectorAll('.pathway .owl-nav')[sliderIndex];
+    let navEl = document.querySelectorAll('.pathway nav')[sliderIndex];
+    let next = navEl.querySelector('.arrow.next');
+    let prev = navEl.querySelector('.arrow.prev');
+
+    // Slider at end?
+    let end = stageEl.querySelector('.owl-next').classList.contains('disabled');
+    // Slider at start?
+    let start = stageEl.querySelector('.owl-prev').classList.contains('disabled');
+
+    if(!end)
+      next.classList.remove('disable');
+    else
+      next.classList.add('disable');
+      
+    if(!start)
+      prev.classList.remove('disable');
+    else
+      prev.classList.add('disable');
+
   }
 
 }
