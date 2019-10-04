e5e29c9c991a13ef5ab056c24bbc7ba0b0177229
Johnny Richardson <johnny.c.rich@gmail.com>
2019-10-04 14:34:29 -0400

    Added new build script for swapping indexes based on type of build.

---
+++ client/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Require arg
+if [  $# -eq 0 ]; then
+    echo "Must run script w/ one arg, either 'qa' or 'production'"
+    exit 1
+fi
+
+echo "Running build"
+
+if [ "$1" == "qa" ]; then
+
+    ng build --configuration=qa-patient --prod --index=src/index.qa.html
+    ng build --configuration=qa-partner --prod --index=src/index.qa.html
+
+    mv dist/patient/index.qa.html dist/patient/index.html
+    mv dist/partner/index.qa.html dist/partner/index.html
+fi
